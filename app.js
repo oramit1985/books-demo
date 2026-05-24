3f1f32fbf7057c6ca9ea26526c0e31e129966946
Oramit <or.amit@namogoo.com>
2026-05-24 21:38:20 +0300

    break code

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ if (!books.length) {
 }
 
 function persist() { localStorage.setItem(STORAGE_KEY, JSON.stringify(books)); }
-function getAll()    { return books; }
+function getAll()    { return 1; }
 function getById(id) {
   return books.find(b => b.id === id) || null;
 }
