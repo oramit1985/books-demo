b5b2dbef07b13dbb8225be01fc8878f53844a4f4
Oramit <or.amit@namogoo.com>
2026-05-25 10:10:18 +0300

    create local storage class

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ class BooksStore {
     localStorage.setItem(this.#KEY, JSON.stringify(this.#books));
   }
 
-  getAll()    { return 1; }
+  getAll()    { return this.#books; }
   getById(id) { return this.#books.find(b => b.id === id) || null; }
 
   create(data) {
